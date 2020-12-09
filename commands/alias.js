3e5c20f509d626f46e820f5ab13a908cc322fb28
Steferson Augusto <steferson_a@hotmail.com>
2020-12-09 14:58:16 -0400

    :bug: corrige problema falha de execução

--- commands/alias.js
+++ commands/alias.js
@@ -4,15 +4,17 @@ const Alias = require('../models/Alias')
 module.exports.run = async (client, msg, args) => {
   try {
     const [user, label] = args
-    const validMember = await check(msg, user, ['master', 'member'])
+    const validMember = await check(user, ['master', 'member'])
     if (validMember) {
-      await Alias.create({ user, label })
+      if (label.toLowerCase() !== 'show') {
+        await Alias.create({ user, label })
+      }
       const aliases = await Alias.findAll({ where: { user }})
       const texto = aliases.reduce((final, alias) => `${final}\n${alias.label}`, '')
       msg.channel.send('```css' + texto + '\n```')
       msg.delete()
     }
-  } catch {
+  } catch(err) {
     msg.channel
       .send('```diff\n- Houve um problema, tenha certeza de que este alias não foi usado anteriormente.\n```')
     msg.delete()
