eabc502eb97749e223d621daa9e41307ad5edfef
Steferson Augusto <steferson_a@hotmail.com>
2021-04-04 21:02:49 -0400

    feat: adiciona comandos mroll e forja_arma

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,22 @@ const { emoji } = require('../utils/services')
 
 module.exports.run = async (client, msg, args) => {
   let embed = new Discord.MessageEmbed()
-	.setColor('#ffeb3b')
 	.setTitle('d!roll')
 	.setDescription(`${emoji('785952478566023178')} Executa rolagem de dados com apliação`)
 	.addFields(
     { name: 'd!roll d12 d6 d6 +4', value: '> Rola d12 + 2d6 +4' },
     { name: 'Resposta: [12, 2, 2]{3} +4: 23', value: '> [rolagens]{ampliações} fixo: resultado final' }
 	)
+
+  msg.channel.send(embed)
+
+  embed = new Discord.MessageEmbed()
+	.setTitle('d!mroll')
+	.setDescription(`${emoji('785952478566023178')} Executa múltiplas rolagens de dados com uma dificuldade`)
+	.addFields(
+    { name: 'd!mroll 20 8 d6 +5 -1', value: '> Executa 20 rolagens de d12 + 2d6 +4 com dificuldade 8' }
+	)
+  .setImage('https://github.com/steferson-augusto/discord-bot-dice/raw/master/github/mroll.png')
 
   msg.channel.send(embed)
 
@@ -36,6 +45,17 @@ module.exports.run = async (client, msg, args) => {
     { name: 'd!anel 80 d8 d8 +2', value: '> Executa 80 tentativas rolando d8 d8 +2' }
   )
   .setImage('https://github.com/steferson-augusto/discord-bot-dice/blob/master/github/anel.png?raw=true')
+  
+  msg.channel.send(embed)
+
+  embed = new Discord.MessageEmbed()
+	.setColor('#FFFA99')
+	.setTitle('d!forja_arma')
+	.setDescription('Forja de armas')
+	.addFields(
+    { name: 'd!forja_arma 50 2 3 1 5d12 2d4 12', value: '> Executa 50 tentativas de forja de arma grau 2, 3 quilos e 1 de tempo dedicado rolando 5d12 2d4 12' }
+  )
+  .setImage('https://github.com/steferson-augusto/discord-bot-dice/blob/master/github/forja_arma.png?raw=true')
   
   msg.channel.send(embed)
 
