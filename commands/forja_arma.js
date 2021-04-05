7caae0c4c4c73cbf0ab7309037f2c6412aeef144
Steferson Augusto <steferson_a@hotmail.com>
2021-04-04 20:57:37 -0400

    feat: adiciona usuário na resposta

--- commands/forja_arma.js
+++ commands/forja_arma.js
@@ -4,9 +4,9 @@ const roll = require('../utils/roll')
 module.exports.run = async (client, msg, args) => {
   const dices = args
 
-  const quantity = Number(dices.shift())
-  const grade = Number(dices.shift())
-  const weight = Number(dices.shift())
+  const quantity = Number(dices.shift()) || 1
+  const grade = Number(dices.shift()) || 1
+  const weight = Number(dices.shift()) || 1
   const relativeTime = Number(dices.shift()) || 0
 
   const minTime = grade * (120 + (15 * grade * 1.1 * (weight - 1)))
@@ -19,19 +19,19 @@ module.exports.run = async (client, msg, args) => {
     if (value > difficulty) success++
   }
   
-
+  const name = msg.author.username
+  const avatarUrl = await msg.author.displayAvatarURL({ format: 'jpg' })
   const embed = new Discord.MessageEmbed()
+  .setAuthor(name, avatarUrl)
 	.setColor('#FFFA99')
 	.setTitle('Forja de armas')
-	// .setDescription('Some description here')
 	.setThumbnail('https://i.imgur.com/eeuZhZy.png')
 	.addField('Tentativas', quantity, true)
 	.addField('Grau', grade, true)
 	.addField('Peso', weight, true)
 	.addField('Dados', dices.join(' '), false)
+	.addField('Dificuldade extra', `${percent * 100 - 100}%`, true)
 	.addField('Dificuldade', difficulty, true)
-	.addField('Bônus', `${percent * 100}%`, true)
-	.addField('\u200B', '\u200B', false)
 	.addField('Acertos', success, false)
 	.setTimestamp()
   msg.channel.send(embed)
